21c0ceda92ec977d06b7c9a3a6204c1cc1c4b9d3
Hasitha Wickramage <wickpra@gmail.com>
2025-11-25 12:59:28 +1300

    1. Created models.py 2. Develop modles for User 3. Created views for user

---
+++ api/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (RegisterView
+ # , ActivityViewSet
+                    )
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+router = DefaultRouter()
+# router.register("activities", ActivityViewSet, basename="activities")
+#Register Url for Activity Create and Update and Delete
+# router.register("activities", ActivityViewSet, basename="activity")
+
+urlpatterns = [
+    path("auth/register/", RegisterView.as_view(), name="auth_register"),
+    path("auth/login/", TokenObtainPairView.as_view(), name="auth_login"),
+    path("auth/refresh/", TokenRefreshView.as_view(), name="auth_refresh"),
+    path("", include(router.urls)),
+]
+
