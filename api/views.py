b325e85905fb4d1dc93e8d01e65f78b1ae0483eb
Piyumi-1996 <piwe001@ess.ais.ac.nz>
2025-11-25 15:37:23 +1300

    1. Created Activity Creation 2. Config serializers.py 3. Config views.py

--- api/views.py
+++ api/views.py
@@ -5,7 +5,7 @@ from rest_framework import generics, viewsets, permissions
 from django.contrib.auth.models import User
 from .models import Activity
 from .serializers import (RegisterSerializer
-# , ActivitySerializer
+, ActivitySerializer
                           )
 
 # REGISTER USER
@@ -14,8 +14,8 @@ class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
     permission_classes = [permissions.AllowAny]
 
-# # CRUD FOR ACTIVITIES
-# class ActivityViewSet(viewsets.ModelViewSet):
-#     queryset = Activity.objects.all().order_by('-timestamp')
-#     serializer_class = ActivitySerializer
-#     permission_classes = [permissions.IsAuthenticated]
+# CRUD FOR ACTIVITIES
+class ActivityViewSet(viewsets.ModelViewSet):
+    queryset = Activity.objects.all().order_by('-timestamp')
+    serializer_class = ActivitySerializer
+    permission_classes = [permissions.IsAuthenticated]
